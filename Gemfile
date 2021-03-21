a8e4c8e5b48c16b9b98901fe2871a62988fa3bd9
Akpagbue Maryjane <akpagbuemaryjane@gmail.com>
2021-03-21 17:50:16 +0100

    Update gemfile

--- Gemfile
+++ Gemfile
@@ -1,40 +1,49 @@
-# frozen_string_literal: true
-
-source 'http://rubygems.org'
+source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.7.2'
 
-# Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
-gem 'rails', '~> 6.1.3'
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '~> 6.0.3', '>= 6.0.3.5'
 # Use postgresql as the database for Active Record
-#gem 'pg', '~> 1.1'
+gem 'pg', '>= 0.18', '< 2.0'
 # Use Puma as the app server
-gem 'puma', '~> 5.0'
-# Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
-# gem 'jbuilder', '~> 2.7'
+gem 'puma', '~> 4.1'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
-# Use Active Model has_secure_password
-gem 'bcrypt', '~> 3.1.7'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
-gem 'rubocop', '~>0.81.0'
 
 # Reduces boot times through caching; required in config/boot.rb
-gem 'bootsnap', '>= 1.4.4', require: false
+gem 'bootsnap', '>= 1.4.2', require: false
 
-gem 'rack-cors', '~> 1.1', '>= 1.1.1'
+gem 'bcrypt', '~> 3.1.7'
+gem 'jwt'
+
+# Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
+gem 'image_processing', '~> 1.2'
+gem 'rack-cors'
 
 group :development, :test do
+  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
+  gem 'database_cleaner'
+  gem 'dotenv-rails'
+  gem 'rspec-rails'
+  gem 'rubocop', '~>0.81.0'
 end
 
-group :production do
-  gem 'pg', '~> 1.1'
+group :development do
+  gem 'listen', '~> 3.2'
+
 end
+
+group :test do
+  gem 'rails-controller-testing'
+  gem 'rspec'
+  gem 'shoulda-matchers'
+end
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
-
-gem 'jwt', '~> 2.2 '
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,19 @@
+# frozen_string_literal: true
+
 doctors = [
-  { 
+  {
     name: 'John Maxwell',
     location: 'Lagos, Nigeria',
-    specialization: "Ophtamology",
+    specialization: 'Ophtamology',
     fees: '25000',
     email: 'johnmaxwell@gmail.com',
     about: 'A Consultant Ear, Nose & Throat Surgeon with over 27 years clinical experience in treating and managing various ear, nose and throat infections.',
     photo: 'https://www.warwickhipkneesurgery.com/wp-content/uploads/2016/09/3313.jpg'
-  },  
+  },
   {
     name: 'Darel Wilson',
     location: 'Delta, Nigeria',
-    specialization: "Dietician",
+    specialization: 'Dietician',
     fees: '20000',
     email: 'darelwilson@gmail.com',
     about: 'A Consultant Ear, Nose & Throat Surgeon with over 27 years clinical experience in treating and managing various ear, nose and throat infections.',
@@ -26,10 +28,10 @@ doctors = [
     about: 'A Consultant Ophthalmologist who is experienced in medical and surgical management of the eye and visual system..',
     photo: 'https://www.andersonmoores.com/uploads/images/8G7VG8KCCHVADBE4V42M.jpeg'
   },
-  { 
+  {
     name: 'Anne Hathaway',
     location: 'Texas US',
-    specialization: "Public Health",
+    specialization: 'Public Health',
     fees: '30000',
     email: 'annehat@gmail.com',
     about: 'I am a Professor of Public Health Opthalmology with 38 years experience in the practice of medicine. The treatment and correction of all eyes related ailments is my specialty. My services include Medical, Surgical and Optical.',
@@ -38,7 +40,7 @@ doctors = [
   {
     name: 'Adebisi Sule',
     location: 'Texas US',
-    specialization: "General Medicine",
+    specialization: 'General Medicine',
     fees: '30000',
     email: 'annehat@gmail.com',
     about: 'I am a Professor of Public Health Opthalmology with 38 years experience in the practice of medicine. The treatment and correction of all eyes related ailments is my specialty. My services include Medical, Surgical and Optical.',
@@ -47,5 +49,5 @@ doctors = [
 ]
 
 doctors.each do |obj|
-   Doctor.create(obj)
+  Doctor.create(obj)
 end
