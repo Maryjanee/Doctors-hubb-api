c2e6fa17d6ec97b8b160a7c85d09dbf3be3c7303
Akpagbue Maryjane <akpagbuemaryjane@gmail.com>
2021-03-23 22:46:49 +0100

    Limit open  ended routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users
-      resources :tokens
-      resources :doctors
-      resources :appointments
+      resources :users, only:[:create]
+      resources :tokens, only: [:create]
+      resources :doctors, only: [:create, :show]
+      resources :appointments, only:[:create, :show, :destroy]
     end
   end
 end
