9699a7714b926baabfe09bda58c50d163924ef18
Akpagbue Maryjane <akpagbuemaryjane@gmail.com>
2021-03-23 12:04:38 +0100

    Update email validation in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :appointments, dependent: :destroy
-  validates :email, uniqueness: true
+  validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
   validates :username, presence: true
-  validates_format_of :email, with: /@/
   validates :password_digest, presence: true
 end
