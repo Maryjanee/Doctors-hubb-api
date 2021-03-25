4a6fe48beaf4092e2a85b8bce08c56285e5de77c
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2021-03-24 22:19:46 -0700

    Fix rubocop errors

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -6,17 +6,19 @@ RSpec.describe Appointment, type: :model do
     @user = User.create(username: 'aldana longhi', email: 'aldulon@gmail.com',
                         password: '123456')
     @doctor = Doctor.create(name: 'John Maxwell',
-      location: 'Lagos, Nigeria',
-      specialization: 'Ophtamology',
-      fees: '25000',
-      email: 'johnmaxwell@gmail.com',
-      about: 'A Consultant Ear, Nose & Throat Surgeon with over 27 years clinical experience in treating and managing various ear, nose and throat infections.',
-      photo: 'https://www.warwickhipkneesurgery.com/wp-content/uploads/2016/09/3313.jpg')
+                            location: 'Lagos, Nigeria',
+                            specialization: 'Ophtamology',
+                            fees: '25000',
+                            email: 'johnmaxwell@gmail.com',
+                            about: 'Throat Surgeon with over 27 years clinical
+                             experience in treating and managing various ear, nose
+                             and throat infections.',
+                            photo: 'https://pic.jpg')
 
     @appointment = @user.appointments.build(doctor_id: @doctor.id, date: '2021-04-30T00:00', city: 'New York')
     @appointment1 = Appointment.create(username: @user.username, doctor_name: @doctor.name,
                                        date: '2021-02-26', city: 'New York', user_id: @user.id,
-                                      doctor_id: @doctor.id)
+                                       doctor_id: @doctor.id)
   end
 
   it 'returns appointment created for a test drive with date after today\'s' do
