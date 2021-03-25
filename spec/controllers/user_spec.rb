0d212f8ca37b2bb4efb118a43da0a6a58967c753
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2021-03-24 20:56:27 -0700

    Add controller test for users

---
+++ spec/controllers/user_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe "API V1 Users", type: 'request' do
+  describe "POST /api/v1/users" do
+    context "with valid parameters" do
+      let(:valid_params) do
+        {
+            username: "Maryjane",
+            email: "maryjane@mail.com",
+            password: "123password"
+          }
+        
+      end
+      it "creates a new user" do
+        expect { post "/api/v1/users", params: valid_params }.to change(User, :count).by(+1)
+        expect(response).to have_http_status :created
+      end
+    end
+
+      
+      it "creates a does not create new user" do
+        expect { post "/api/v1/users", params: { username: "Maryjane",
+          email: "maryjane.com",
+          password: "123password"
+        }}.to change(User, :count).by(0)
+      end
+
+
+  end
+end
